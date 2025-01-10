4c6b3617568b900b8b2ab164ceabd0941027d5d9
joe baudisch <josef.baudisch@frm2.tum.de>
2025-01-10 07:47:37 +0100

    updating project for a local cli version
    Change-Id: Ib8b19dfe652234ec7e9f4f77c59690cce1c50aa2

--- joeselnfrontend/src/environments/environment.prod.ts
+++ joeselnfrontend/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+    production: true,
+  apiUrl: 'http://172.25.74.236:8010',
+  wsUrl: 'ws://172.25.74.236:8010/ws',
+  tracking: false,
+  matomoUrl: null,
+  matomoId: null,
+  labBookSocketRefreshInterval: 1000,
+  eln_exporter: 'http://172.25.74.236:5000',
+  keycloak_url: 'http://daphneopc01:8181/',
+  keycloak_realm: 'joe',
+  keycloak_clientId: 'my_client'
 };
