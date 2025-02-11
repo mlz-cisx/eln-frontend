e14858210d628e4489441b289a0a1332db3cbfae
joe baudisch <josef.baudisch@frm2.tum.de>
2025-02-11 15:38:59 +0100

    login page refactoring
    Change-Id: I5ae39116cb85b2b429f09c08fec2f4c3003f83ce

--- joeselnfrontend/src/app/pages/login/components/login-page/login-page.component.ts
+++ joeselnfrontend/src/app/pages/login/components/login-page/login-page.component.ts
@@ -1,34 +1,15 @@
-/**
- * Copyright (C) 2016-2020 TU Muenchen and contributors of ANEXIA Internetdienstleistungs GmbH
- * SPDX-License-Identifier: AGPL-3.0-or-later
- */
-
 import {
   ChangeDetectionStrategy,
-  ChangeDetectorRef,
   Component,
   OnInit,
 } from '@angular/core';
 import {Validators, UntypedFormBuilder, UntypedFormGroup} from '@angular/forms';
 import {Title} from '@angular/platform-browser';
-import {ActivatedRoute, Router, RouterStateSnapshot} from '@angular/router';
-// import { AuthService, PageTitleService } from '@app/services';
-// import type { UserState } from '@app/stores/user';
-// import { EmailDetected } from '@app/validators/email-detected.validator';
-// import { FormBuilder, FormControl } from '@ngneat/reactive-forms';
-import {TranslocoService} from '@ngneat/transloco';
 import {UntilDestroy, untilDestroyed} from '@ngneat/until-destroy';
-import {map, take} from 'rxjs/operators';
 import {UserService} from "@app/services";
-import {AuthService} from "@app/services";
 import {KeycloakService} from 'keycloak-angular';
 import {environment} from "@environments/environment";
 
-
-// interface FormLogin {
-//   username: FormControl<string | null>;
-//   password: FormControl<string | null>;
-// }
 
 @UntilDestroy()
 @Component({
@@ -44,54 +25,21 @@ export class LoginPageComponent implements OnInit {
 
   public keycloak_integration = environment.keycloak_integration;
 
-  // public form = this.fb.group<FormLogin>({
-  //   username: this.fb.control(null, [Validators.required]),
-  //   password: this.fb.control(null, Validators.required),
-  // });
-
-  // public form = this.fb.group<FormLogin>({
-  //   username: this.fb.control(null, [Validators.required]),
-  //   password: this.fb.control(null, Validators.required),
-  // });
 
   public form!: UntypedFormGroup;
 
-  private returnUrl = '/';
 
   public constructor(
-    // private readonly route: ActivatedRoute,
-    // private readonly router: Router,
     private readonly fb: UntypedFormBuilder,
-    // private readonly authService: AuthService,
-    // private readonly translocoService: TranslocoService,
-    // private readonly cdr: ChangeDetectorRef,
-    // private readonly pageTitleService: PageTitleService,
     private readonly titleService: Title,
     private user_service: UserService,
     public keycloak: KeycloakService,
   ) {
   }
 
-  // public get f() {
-  //   return this.form.controls;
-  // }
 
   public ngOnInit(): void {
 
-    // this.authService.user$.pipe(
-    //   untilDestroyed(this),
-    //   take(1),
-    //   map((user: UserState) => {
-    //     if (user.loggedIn) {
-    //       void this.router.navigate(['/']);
-    //     }
-    //   })
-    // );
-
-
-    // this.initTranslations();
-    // this.initPageTitle();
-    // void this.pageTitleService.set(this.title);
 
     this.form = this.fb.group({
       username: ['', Validators.required],
@@ -99,44 +47,11 @@ export class LoginPageComponent implements OnInit {
     });
   }
 
-  // public initTranslations(): void {
-  //   this.translocoService
-  //     .selectTranslate('login.title')
-  //     .pipe(untilDestroyed(this))
-  //     .subscribe(title => {
-  //       this.title = title;
-  //       // void this.pageTitleService.set(title);
-  //     });
-  // }
 
   public initPageTitle(): void {
-    // this.pageTitleService
-    //   .get()
-    //   .pipe(untilDestroyed(this))
-    //   .subscribe(title => {
-    //     this.titleService.setTitle(title);
-    //   });
+
   }
 
-  // public onLogin(): void {
-  //   if (this.loading || this.form.invalid) {
-  //     return;
-  //   }
-  //   this.loading = true;
-  //
-  //   // this.authService
-  //   //   .login(this.f.username.value!, this.f.password.value!)
-  //   //   .pipe(untilDestroyed(this))
-  //   //   .subscribe(
-  //   //     () => {
-  //   //       void this.router.navigate([this.returnUrl]);
-  //   //     },
-  //   //     () => {
-  //   //       this.loading = false;
-  //   //       this.cdr.markForCheck();
-  //   //     }
-  //   //   );
-  // }
 
   public login() {
     // @ts-ignore
--- joeselnfrontend/src/app/pages/login/login-page.module.ts
+++ joeselnfrontend/src/app/pages/login/login-page.module.ts
@@ -1,19 +1,15 @@
-/**
- * Copyright (C) 2016-2020 TU Muenchen and contributors of ANEXIA Internetdienstleistungs GmbH
- * SPDX-License-Identifier: AGPL-3.0-or-later
- */
-
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
-import { FormHelperModule } from '@app/modules/form-helper/form-helper.module';
-// import { HeaderModule } from '@app/modules/header/header.module';
-import { CustomControlErrorComponent } from '@app/modules/shared/control-error/control-error.component';
-import { TranslocoRootModule } from '@app/transloco-root.module';
-import { FormsModule } from '@joeseln/forms';
-import { ErrorTailorModule } from '@ngneat/error-tailor';
-import { TranslocoService } from '@ngneat/transloco';
-import { LoginPageComponent } from './components/login-page/login-page.component';
-import { LoginPageRoutingModule } from './login-page-routing.module';
+import {CommonModule} from '@angular/common';
+import {NgModule} from '@angular/core';
+import {FormHelperModule} from '@app/modules/form-helper/form-helper.module';
+import {
+  CustomControlErrorComponent
+} from '@app/modules/shared/control-error/control-error.component';
+import {TranslocoRootModule} from '@app/transloco-root.module';
+import {FormsModule} from '@joeseln/forms';
+import {ErrorTailorModule} from '@ngneat/error-tailor';
+import {TranslocoService} from '@ngneat/transloco';
+import {LoginPageComponent} from './components/login-page/login-page.component';
+import {LoginPageRoutingModule} from './login-page-routing.module';
 
 @NgModule({
   declarations: [LoginPageComponent],
@@ -21,7 +17,6 @@ import { LoginPageRoutingModule } from './login-page-routing.module';
     CommonModule,
     LoginPageRoutingModule,
     TranslocoRootModule,
-    // HeaderModule,
     FormsModule,
     FormHelperModule,
     ErrorTailorModule.forRoot({
@@ -38,4 +33,5 @@ import { LoginPageRoutingModule } from './login-page-routing.module';
     }),
   ],
 })
-export class LoginPageModule {}
+export class LoginPageModule {
+}
