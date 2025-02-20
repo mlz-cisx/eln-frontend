0ffe0d02bdbd5b7db28daf1314aa9f938cc1c33d
wojian wang <wojian.wang@frm2.tum.de>
2025-02-20 11:09:30 +0100

    ui: admin-page

--- joeselnfrontend/src/environments/environment.ts
+++ joeselnfrontend/src/environments/environment.ts
@@ -1,14 +1,14 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://172.25.74.236:8010/api',
-  wsUrl: 'ws://172.25.74.236:4501/ws',
+  apiUrl: 'http://wojians-macbook.local:8010/api',
+  wsUrl: 'ws://wojians-macbook.local:8010/ws',
   tracking: false,
   matomoUrl: null,
   matomoId: null,
   labBookSocketRefreshInterval: 1000,
   eln_exporter: 'http://172.25.74.236:5000',
-  keycloak_url: 'http://daphneopc01:8082/',
+  keycloak_url: 'http://wojians-macbook.local:8080/',
   keycloak_realm: 'joe',
-  keycloak_clientId: 'my_client',
-  keycloak_integration: true
+  keycloak_clientId: 'client_frontend',
+  keycloak_integration: true,
 };
