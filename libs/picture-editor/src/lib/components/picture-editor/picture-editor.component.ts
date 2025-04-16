13cd535e366d8d6131611b666c9767cb46761fce
wojian wang <wojian.wang@frm2.tum.de>
2025-04-16 13:04:53 +0200

    angular zoneless change detection
    Change-Id: Ia7b15b3c3181b17aa19dc65ae2a95eaf14c7ab85
    Reviewed-on: https://forge.frm2.tum.de/review/c/mlz/eln/frontend/+/36067
    Reviewed-by: Josef Baudisch <josef.baudisch@frm2.tum.de>
    Tested-by: Wojian Wang <wojian.wang@frm2.tum.de>

--- libs/picture-editor/src/lib/components/picture-editor/picture-editor.component.ts
+++ libs/picture-editor/src/lib/components/picture-editor/picture-editor.component.ts
@@ -13,7 +13,6 @@ import {
   Inject,
   InjectionToken,
   Input,
-  NgZone,
   OnDestroy,
   OnInit,
   Optional,
@@ -72,7 +71,6 @@ export class PictureEditorComponent implements OnInit, AfterViewInit, OnDestroy 
 
   public constructor(
     private readonly cdr: ChangeDetectorRef,
-    private readonly ngZone: NgZone,
     @Inject(DOCUMENT) private readonly document: Document,
     @Optional() @Inject(PICTURE_EDITOR_SCRIPT_SRC) private readonly pictureEditorScriptSrc?: string
   ) {
@@ -107,26 +105,24 @@ export class PictureEditorComponent implements OnInit, AfterViewInit, OnDestroy 
       this.backgroundImage.src = this.picture.download_background_image;
     }
 
-    this.ngZone.runOutsideAngular(() => {
-      this.canvas = new window.CH.CanvasHelper(this.core.nativeElement, {
-        ...window.CH.defaultOptions,
-        imageSize: {
-          width: this.picture.width ?? 600,
-          height: this.picture.height ?? 600,
-        },
-        toolbarPosition: 'hidden',
-        strokeWidths: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30],
-        defaultStrokeWidth: stroke_width,
-        tools: [window.CH.SelectShape],
-        backgroundShapes:
-          !this.sketch && this.picture.download_background_image
-            ? [window.CH.shapes.createShape('Image', {x: 0, y: 0, image: this.backgroundImage, scale: 1})]
-            : [],
-      });
-      this.canvas.setColor('primary', '#000000');
-      this.canvas.setColor('secondary', '#00000000');
-      this.canvas.undoStack = [];
+    this.canvas = new window.CH.CanvasHelper(this.core.nativeElement, {
+      ...window.CH.defaultOptions,
+      imageSize: {
+        width: this.picture.width ?? 600,
+        height: this.picture.height ?? 600,
+      },
+      toolbarPosition: 'hidden',
+      strokeWidths: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30],
+      defaultStrokeWidth: stroke_width,
+      tools: [window.CH.SelectShape],
+      backgroundShapes:
+        !this.sketch && this.picture.download_background_image
+          ? [window.CH.shapes.createShape('Image', {x: 0, y: 0, image: this.backgroundImage, scale: 1})]
+          : [],
     });
+    this.canvas.setColor('primary', '#000000');
+    this.canvas.setColor('secondary', '#00000000');
+    this.canvas.undoStack = [];
 
     if (!this.sketch && this.picture.download_shapes) {
       this.service
