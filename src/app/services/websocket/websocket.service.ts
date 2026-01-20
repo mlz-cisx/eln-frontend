5d2926006d8feb45c2bc1410efe60766dd6cc2ae
joe baudisch <josef.baudisch@frm2.tum.de>
2026-01-20 11:13:03 +0100

    make ws more robust
    Change-Id: Iead76c572ad8ae3f79f9d971fe545e7fd01c79a7
    Reviewed-on: https://forge.frm2.tum.de/review/c/mlz/eln/frontend/+/38271
    Reviewed-by: Wojian Wang <wojian.wang@frm2.tum.de>
    Tested-by: Josef Baudisch <josef.baudisch@frm2.tum.de>

--- src/app/services/websocket/websocket.service.ts
+++ src/app/services/websocket/websocket.service.ts
@@ -1,42 +1,46 @@
 import {Injectable} from '@angular/core';
 import {environment} from '@environments/environment';
-import {Subject} from 'rxjs';
-import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
+import {Subject, Subscription} from 'rxjs';
+import {webSocket, WebSocketSubject} from 'rxjs/webSocket';
 import {AuthService} from "@app/services";
-
 
 
 @Injectable({
   providedIn: 'root',
 })
 export class WebSocketService {
-  private socket$?: WebSocketSubject<any>; // eslint-disable-line
-
-  public elements = new Subject();
-
 
   public constructor(
     private _auth: AuthService,
   ) {
   }
 
+  public elements = new Subject<any>();
+  private socket$?: WebSocketSubject<any>;
+  private socketSub?: Subscription;
+
   public connect(): void {
-
-    if (!this.socket$ || this.socket$.closed) {
-      let token = '';
-      if (this._auth.getToken()) {
-        token = 'jwt_' + this._auth.getToken();
-      } else return;
-
-      this.socket$ = webSocket(`${environment.wsUrl}/${token}`);
-    }
-
-    this.socket$.subscribe({ next: (msg) => this.elements.next(msg) });
-  }
-
-  public close() {
+    // Close old socket
     if (this.socket$ && !this.socket$.closed) {
       this.socket$.complete();
     }
+    this.socketSub?.unsubscribe();
+
+    const token = this._auth.getToken();
+    if (!token) return;
+
+    this.socket$ = webSocket(`${environment.wsUrl}/jwt_${token}`);
+
+    this.socketSub = this.socket$.subscribe({
+      next: msg => this.elements.next(msg),
+      error: err => console.error('WS error', err),
+      complete: () => console.log('WS closed')
+    });
   }
+
+  public close(): void {
+    this.socketSub?.unsubscribe();
+    this.socket$?.complete();
+  }
+
 }
